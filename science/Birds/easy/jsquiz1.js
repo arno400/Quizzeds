1831d5cd056e7680e752a25eea5effbff2c6f591
Aarav Ahluwalia <87759306+arno400@users.noreply.github.com>
2022-12-20 19:29:46 +0530

    Add files via upload

---
+++ science/Birds/easy/jsquiz1.js
@@ -0,0 +1,181 @@
+(function() {
+    var questions = [{
+      
+      question: "Birds reproduce by which of the following?",
+      choices: ["laying eggs", "giving birth to young ones", "both a and b", "none of these"],
+      correctAnswer: 0
+    }, {
+      question: "Birds have _____ bones",
+      choices: ["hollow", "solid", "heavy", "all of these"],
+      correctAnswer: 0
+    }, {
+      question: "Birds sit on thir eggs to keep them _____",
+      choices: ["cold", "warm", "hot", "cool"],
+      correctAnswer: 1
+    }, {
+        question: "Bird's _____ and _____ let us know about their eating habits",
+      choices: ["claws,tail", "wings,tail", "claws,beak", "wings,beak"],
+      correctAnswer: 3
+
+    }, {
+      question: "Birds have _____ body",
+      choices: ["hardest", "heaviest", "streamline", "streamline"],
+      correctAnswer: 2
+    }, {
+      question: "birds are _____ blooded animals?",
+      choices: ["cool", "warm", "cold", "hot"],
+      correctAnswer: 1
+    }, {
+        question: "Birds' soft down feathers help them to keep _____",
+      choices: ["cool", "cold", "warm", "hot"],
+      correctAnswer: 2
+    }, {
+      question: "Birds' long feathers help them to _____",
+      choices: ["eat their prey", "speak", "crash", "fly"],
+      correctAnswer: 3
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' answers right!!! out of ' +
+                   questions.length + ' ');
+      return score;
+    }
+  })();
---
+++ science/Human body/easy/jsquiz1.js
@@ -0,0 +1,181 @@
+(function() {
+    var questions = [{
+      
+      question: "Which part of body absorb water from the food that we eat?",
+      choices: ["small intestine", "stomach", "large intestine", "heart"],
+      correctAnswer: 2
+    }, {
+      question: "Sense organ that helps the body to keep its balance?",
+      choices: ["ears", "eyes", "nose", "skin"],
+      correctAnswer: 0
+    }, {
+      question: "This J-shaped organ helps to digest food?",
+      choices: ["anus", "liver", "large intestine", "stomach"],
+      correctAnswer: 3
+    }, {
+        question: "how many percent of brain is made up of water?",
+      choices: [25, 75, 100, 10],
+      correctAnswer: 1
+
+    }, {
+        question: "This is the long tube that absorb nutrients from the food that we eat?",
+      choices: ["small intestine", "liver", "large intestine", "urinary bladder"],
+      correctAnswer: 0
+    }, {
+        question: "These are cone shaped organs help in breathing?",
+      choices: ["heart", "nose", "lungs", "kidney"],
+      correctAnswer: 2
+    }, {
+        question: "Which organ produces digestive juice called bile?",
+      choices: ["anus", "liver", "large intestine", "stomach"],
+      correctAnswer: 1
+    }, {
+      question: "Which organ stores consentrated bile?",
+      choices: ["small intestine", "stomach", "large intestine", "gall bladder"],
+      correctAnswer: 3
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' answers right!!! out of ' +
+                   questions.length + ' ');
+      return score;
+    }
+  })();
